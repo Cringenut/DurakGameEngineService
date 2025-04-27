c77460d1f0550d65ef9b14af8e5516c93863d0e1
Cringenut <ihnatbaikou@gmail.com>
2025-04-27 20:02:53 +0200

    Initial commit

---
+++ src/test/java/com/cringenut/game_engine_service/GameEngineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cringenut.game_engine_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameEngineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
